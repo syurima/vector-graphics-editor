a3c97e5a7bf1a85f89fa8cbf30afaa7c8a7a5e78
syurima <natka.dys@gmail.com>
2025-04-03 21:36:21 +0200

    refactor, set minimum window size

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 import java.awt.*;
-
 import javax.swing.*;
 
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.PrintWriter;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.io.*;
 import javax.imageio.*;
-
-
 
 enum ShapeType {
     LINE, RECTANGLE, CIRCLE
@@ -22,7 +18,8 @@ enum OperationType {
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(VectorGraphicsEditorWindow::new);
+        VectorGraphicsEditorWindow window = new VectorGraphicsEditorWindow();
+        window.setVisible(true);
     }
 }
 
@@ -30,6 +27,7 @@ class VectorGraphicsEditorWindow extends JFrame {
     public VectorGraphicsEditorWindow() {
         setTitle("Vector Graphics Editor");
         setSize(800, 600);
+        setMinimumSize(new Dimension(800, 600));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         VectorGraphicsEditor editorPanel = new VectorGraphicsEditor();
@@ -62,25 +60,6 @@ class VectorGraphicsEditor extends JPanel {
         // Control panel
         controlPanel = new ControlPanel();
         add(controlPanel, BorderLayout.SOUTH);
-    }
-
-    public void setColor() {
-        try {
-            int r = Integer.parseInt(controlPanel.rField.getText());
-            int g = Integer.parseInt(controlPanel.gField.getText());
-            int b = Integer.parseInt(controlPanel.bField.getText());
-
-            // // Clamp values to 0-255
-            // r = Math.max(0, Math.min(255, r));
-            // g = Math.max(0, Math.min(255, g));
-            // b = Math.max(0, Math.min(255, b));
-            
-            currentColor = new Color(r, g, b);
-            controlPanel.colorPreview.setBackground(currentColor); // Update preview
-        } catch (Exception e) { 
-            // Values are not integers or out of range
-            JOptionPane.showMessageDialog(this, "Invalid color values. Please enter integers between 0 and 255.");
-        }
     }
 
     private class DrawPanel extends JPanel 
@@ -405,6 +384,25 @@ class VectorGraphicsEditor extends JPanel {
             colorPreview.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             this.add(colorPreview);
         }
+
+        public void setColor() {
+            try {
+                int r = Integer.parseInt(controlPanel.rField.getText());
+                int g = Integer.parseInt(controlPanel.gField.getText());
+                int b = Integer.parseInt(controlPanel.bField.getText());
+    
+                // // Clamp values to 0-255
+                // r = Math.max(0, Math.min(255, r));
+                // g = Math.max(0, Math.min(255, g));
+                // b = Math.max(0, Math.min(255, b));
+                
+                currentColor = new Color(r, g, b);
+                colorPreview.setBackground(currentColor); // Update preview
+            } catch (Exception e) { 
+                // Values are not integers or out of range
+                JOptionPane.showMessageDialog(this, "Invalid color values. Please enter integers between 0 and 255.");
+            }
+        }
     }
 
     private class OperationPanel extends JPanel {
@@ -444,160 +442,5 @@ class VectorGraphicsEditor extends JPanel {
             this.add(loadButton);
             this.add(clearButton);
         }
-    }
-}
-
-interface Shape {
-    void draw(Graphics g);
-    String toString();                      // Serialize the shape to a string representation
-    static Shape fromString(String s) {     // Deserialize the shape from a string representation
-        try{
-            // split the string into parts
-            String[] s_split = s.split(" ");
-            String shapeType = s_split[0];
-            int x = Integer.parseInt(s_split[1]);
-            int y = Integer.parseInt(s_split[2]);
-            String color = s_split[s_split.length - 1];
-            switch (shapeType) {
-                case "LINE":
-                    Point start = new Point(x, y);
-                    Point end = new Point(Integer.parseInt(s_split[3]), Integer.parseInt(s_split[4]));
-                    return new Line(start, end, new Color(Integer.parseInt(color)));
-                case "RECTANGLE":
-                    int width = Integer.parseInt(s_split[3]);
-                    int height = Integer.parseInt(s_split[4]);
-                    return new Rect(new Point(x, y), new Point(x + width, y + height), new Color(Integer.parseInt(color)));
-                case "CIRCLE":
-                    int radius = Integer.parseInt(s_split[3]);
-                    return new Circle(new Point(x, y), radius, new Color(Integer.parseInt(color)));
-                default:
-                    return null; // Unknown shape type
-            }
-        }
-        catch (Exception e) {
-            return null; // Return null if the string is not a valid shape representation
-        }
-    }
-    Point getCenter();
-    public void move(int dx, int dy);
-}
-
-class Line implements Shape {
-    private final Point start, end;
-    private final Point center;
-    private final Color color;
-
-    public Line(Point start, Point end, Color color) {
-        this.start = start;
-        this.end = end;
-        this.center = new Point((start.x + end.x) / 2, (start.y + end.y) / 2);
-        this.color = color;
-    }
-    
-    @Override
-    public void draw(Graphics g) {
-        g.setColor(color);
-        g.drawLine(start.x, start.y, end.x, end.y);
-    }
-
-    @Override
-    public String toString() {
-        return "LINE " + start.x + " " + start.y + " " + end.x + " " + end.y + " " + color.getRGB();
-    }
-    @Override
-    public Point getCenter() {
-        return center;
-    }
-    public Point getStart() {
-        return start;
-    }
-    public Point getEnd() {
-        return end;
-    }
-
-    @Override
-    public void move(int dx, int dy) {
-        start.translate(dx, dy);
-        end.translate(dx, dy);
-        center.translate(dx, dy);
-    }
-    public void recalculateCenter() {
-        center.setLocation((start.x + end.x) / 2, (start.y + end.y) / 2);
-    }
-}
-
-class Rect implements Shape {
-    private final Rectangle rect;
-    private final Point center;
-    private final Color color;
-
-    public Rect(Point start, Point end, Color color) {
-        int x = Math.min(start.x, end.x);
-        int y = Math.min(start.y, end.y);
-        int width = Math.abs(start.x - end.x);
-        int height = Math.abs(start.y - end.y);
-        this.rect = new Rectangle(x, y, width, height);
-        this.center = new Point(x + width / 2, y + height / 2);
-        this.color = color;
-    }
-
-    @Override
-    public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillRect(rect.x, rect.y, rect.width, rect.height);
-    }
-
-    @Override
-    public String toString() {
-        return "RECTANGLE " + rect.x + " " + rect.y + " " + rect.width + " " + rect.height + " " + color.getRGB();
-    }
-    @Override
-    public Point getCenter() {
-        return center;
-    }
-    @Override
-    public void move(int dx, int dy) {
-        rect.translate(dx, dy);
-        center.translate(dx, dy);
-    }
-}
-
-class Circle implements Shape {
-    private final Point center;
-    private final int radius;
-    private final Color color;
-
-    public Circle(Point center, Point edge, Color color) {
-        int dx = edge.x - center.x;
-        int dy = edge.y - center.y;
-        this.radius = (int) Math.sqrt(dx * dx + dy * dy);
-        this.center = center;
-        this.color = color;
-    }
-
-    public Circle(Point center, int radius, Color color) {
-        this.center = center;
-        this.radius = radius;
-        this.color = color;
-    }
-
-    @Override
-    public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillOval(center.x - radius, center.y - radius, radius * 2, radius * 2);
-    }
-
-    @Override
-    public String toString() {
-        return "CIRCLE " + (center.x) + " " + (center.y) + " " + radius + " " + color.getRGB();
-    }
-
-    @Override
-    public Point getCenter() {
-        return center;
-    }
-    @Override
-    public void move(int dx, int dy) {
-        center.translate(dx, dy);
     }
 }
---
+++ src/Shape.java
@@ -0,0 +1,157 @@
+import java.awt.*;
+
+
+interface Shape {
+    void draw(Graphics g);
+    String toString();                      // Serialize the shape to a string representation
+    static Shape fromString(String s) {     // Deserialize the shape from a string representation
+        try{
+            // split the string into parts
+            String[] s_split = s.split(" ");
+            String shapeType = s_split[0];
+            int x = Integer.parseInt(s_split[1]);
+            int y = Integer.parseInt(s_split[2]);
+            String color = s_split[s_split.length - 1];
+            switch (shapeType) {
+                case "LINE":
+                    Point start = new Point(x, y);
+                    Point end = new Point(Integer.parseInt(s_split[3]), Integer.parseInt(s_split[4]));
+                    return new Line(start, end, new Color(Integer.parseInt(color)));
+                case "RECTANGLE":
+                    int width = Integer.parseInt(s_split[3]);
+                    int height = Integer.parseInt(s_split[4]);
+                    return new Rect(new Point(x, y), new Point(x + width, y + height), new Color(Integer.parseInt(color)));
+                case "CIRCLE":
+                    int radius = Integer.parseInt(s_split[3]);
+                    return new Circle(new Point(x, y), radius, new Color(Integer.parseInt(color)));
+                default:
+                    return null; // Unknown shape type
+            }
+        }
+        catch (Exception e) {
+            return null; // Return null if the string is not a valid shape representation
+        }
+    }
+    Point getCenter();
+    public void move(int dx, int dy);
+}
+
+class Line implements Shape {
+    private final Point start, end;
+    private final Point center;
+    private final Color color;
+
+    public Line(Point start, Point end, Color color) {
+        this.start = start;
+        this.end = end;
+        this.center = new Point((start.x + end.x) / 2, (start.y + end.y) / 2);
+        this.color = color;
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.drawLine(start.x, start.y, end.x, end.y);
+    }
+
+    @Override
+    public String toString() {
+        return "LINE " + start.x + " " + start.y + " " + end.x + " " + end.y + " " + color.getRGB();
+    }
+    @Override
+    public Point getCenter() {
+        return center;
+    }
+    public Point getStart() {
+        return start;
+    }
+    public Point getEnd() {
+        return end;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        start.translate(dx, dy);
+        end.translate(dx, dy);
+        center.translate(dx, dy);
+    }
+    public void recalculateCenter() {
+        center.setLocation((start.x + end.x) / 2, (start.y + end.y) / 2);
+    }
+}
+
+class Rect implements Shape {
+    private final Rectangle rect;
+    private final Point center;
+    private final Color color;
+
+    public Rect(Point start, Point end, Color color) {
+        int x = Math.min(start.x, end.x);
+        int y = Math.min(start.y, end.y);
+        int width = Math.abs(start.x - end.x);
+        int height = Math.abs(start.y - end.y);
+        this.rect = new Rectangle(x, y, width, height);
+        this.center = new Point(x + width / 2, y + height / 2);
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    @Override
+    public String toString() {
+        return "RECTANGLE " + rect.x + " " + rect.y + " " + rect.width + " " + rect.height + " " + color.getRGB();
+    }
+    @Override
+    public Point getCenter() {
+        return center;
+    }
+    @Override
+    public void move(int dx, int dy) {
+        rect.translate(dx, dy);
+        center.translate(dx, dy);
+    }
+}
+
+class Circle implements Shape {
+    private final Point center;
+    private final int radius;
+    private final Color color;
+
+    public Circle(Point center, Point edge, Color color) {
+        int dx = edge.x - center.x;
+        int dy = edge.y - center.y;
+        this.radius = (int) Math.sqrt(dx * dx + dy * dy);
+        this.center = center;
+        this.color = color;
+    }
+
+    public Circle(Point center, int radius, Color color) {
+        this.center = center;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(center.x - radius, center.y - radius, radius * 2, radius * 2);
+    }
+
+    @Override
+    public String toString() {
+        return "CIRCLE " + (center.x) + " " + (center.y) + " " + radius + " " + color.getRGB();
+    }
+
+    @Override
+    public Point getCenter() {
+        return center;
+    }
+    @Override
+    public void move(int dx, int dy) {
+        center.translate(dx, dy);
+    }
+}
